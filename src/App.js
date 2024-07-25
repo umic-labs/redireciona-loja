f50747a2e1e328479eac5e9c08d0090e6e6704ac
Igor Aragão <roginaldosemog@gmail.com>
2024-07-24 23:17:16 -0300

    Redirect root to mercado shops

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function RedirectToExternalURL() {
+  useEffect(() => {
+    window.location.href = 'https://umic.mercadoshops.com.br/';
+  }, []);
+
+  return null;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<RedirectToExternalURL />} />
+      </Routes>
+    </Router>
   );
 }
 
